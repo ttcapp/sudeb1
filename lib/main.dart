5a3b9e776933b962c52f03fb84f07b05f1f110cd
Emam999 <mdemamshikder1@gmail.com>
2021-10-23 00:37:46 -0600

    first commit1st

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,27 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("LMC Center"),
+      ),
+    );
+  }
+}
+
+
