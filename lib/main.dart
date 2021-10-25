f0e1a641f635423f36ddc4521ffff7c6ffc3ced1
Emam999 <mdemamshikder1@gmail.com>
2021-10-24 23:15:44 -0600

    first commit2nd

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+String imgSrcs1="https://thumbs.dreamstime.com/b/yellow-orange-starburst-flower-nature-jpg-192959431.jpg";
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -18,9 +19,30 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height=MediaQuery.of(context).size.height;
+    double width=MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("LMC Center"),
+      ),
+      body: Center(
+        child: Container(
+          height: height,
+          width: width,
+          color: Colors.purpleAccent,
+          child: Column(
+            children: [
+
+              Text("First App",style: TextStyle(
+                fontSize: 30,fontWeight: FontWeight.bold
+              ),),
+              Container(
+                height: height/3,
+                  width: width/2,
+                  child: Image.network(imgSrcs1)),
+            ],
+          ),
+        ),
       ),
     );
   }
